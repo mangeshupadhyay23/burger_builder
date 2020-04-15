a9fcfc81d8e74750bfa849ebcc098aac88a7feaa
mangesh <mshanit@gmail.com>
2020-04-15 22:17:00 +0530

    Added dynamical input form

---
+++ src/components/UI/input/input.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './input.css';
+
+const input=(props)=>{
+
+    let inputElement=null;
+    switch(props.inputtype){
+        case('input'):
+            inputElement=<input className='InputElement' {...props.elementConfig} value={props.value}/>;
+            break;
+        case('textarea'):
+            inputElement=<textarea className='InputElement' {...props.elementConfig} value={props.value}/>;
+            break;
+        case('select'):
+            inputElement=(
+            <select
+             className='InputElement'  
+             value={props.value}>
+                {props.elementConfig.options.map(option=>(
+                    <option value={option.value}>
+                        {option.displayValue}
+                        </option>
+                ))}
+            </select>);
+            break;
+        default:
+            inputElement=<input className='InputElement' {...props.elementConfig} value={props.value}/>;
+            break;
+            
+    }
+
+    return(
+            <div className='Input'>
+                <label className='Label'>
+                    {props.label}
+                </label>
+                {inputElement}
+
+            </div>
+    );
+}
+export default input;
--- src/container/checkout/Contact Data/Contact Data.js
+++ src/container/checkout/Contact Data/Contact Data.js
@@ -2,17 +2,64 @@ import React from 'react';
 import Button from '../../../components/UI/button/success button';
 import './Contact Data.css';
 import Spinner from '../../../components/UI/spinner/spinner';
-import axios from '../../../axios-orders'
+import axios from '../../../axios-orders';
+import Input from '../../../components/UI/input/input';
 
 
 class ContactData extends React.Component{
     state={
-        name:'',
-        email:'',
-        address:{
-            street:'',
-            postalCode:''
-        },
+        orderform:{
+            name:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Your Name'
+                },
+                value:''
+            },
+            street:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Street Name'
+                },
+                value:''
+            },
+            zipcode:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'ZIP Code'
+                },
+                value:''
+            },
+            country:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Country'
+                },
+                value:''
+            },
+            email:{
+                elementType:'input',
+                elementConfig:{
+                    type:'email',
+                    placeholder:'Your E-mail'
+                },
+                value:''
+            },
+            deliveryMethod:{
+                elementType:'select',
+                elementConfig:{
+                   options:[
+                       {value:'fastest',displayValue:'Fastest'},
+                       {value:'cheapest',displayValue:'Cheapest'}, 
+                   ]
+                },
+                value:''
+            }
+        },  
         loading:false
     }
     orderHandler=(event)=>{
@@ -22,16 +69,7 @@ class ContactData extends React.Component{
         const order={
             ingredients:this.props.ingredients,
             price:this.props.price,
-            customer:{
-                name:'Mangesh Upadhyay',
-                address:{
-                    houseno :"272/32",
-                    colony:'Vivek vihar',
-                    district:'Karauli',
-                    state:'Rajasthan'
-                }
-            },
-            deliveryMethod:'fastest'
+           
         }
         axios.post('/orders.json',order)
              .then(response=>{
@@ -45,15 +83,25 @@ class ContactData extends React.Component{
         
     }
     render(){
+        const formElementsArray=[];
+        for (let key in this.state.orderform){
+            formElementsArray.push({
+                id:key,
+                config:this.state.orderform[key]
+            })
+        }
+
+
+
         let form=(<form>
-            <input type='text'  name='name' placeholder='Your Name'></input>
-            <br/>
-            <input type='email'  name='email' placeholder='Your Mail'></input>
-            <br/>
-            <input type='text'  name='street' placeholder='Street'></input>
-            <br/>
-            <input type='text'  name='postal code' placeholder='Postal Code'></input>
-            <br/>
+            {formElementsArray.map(formElement=>(
+                <Input 
+                 key={formElement.id}
+                 elementType={formElement.config.elementType}
+                 elementConfig={formElement.config.elementConfig}
+                 value={formElement.config.value}
+                 />
+            ))}
             <Button clicked={this.orderHandler}>ORDER</Button>
         </form> );
         if(this.state.loading){
